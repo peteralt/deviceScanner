21789d354ab28f943d5c19f16bc018385501e83b
Peter Alt <peter@peteralt.com>
2017-01-17 20:30:25 -0500

    loading settings from config file, adding known devices, posting status to slack

--- deviceScanner.py
+++ deviceScanner.py
@@ -1,19 +1,27 @@
 import nmap, requests, json, logging
 from pymongo import MongoClient
 from datetime import datetime
+from pprint import pprint
+import slackweb
 
 class deviceScanner:
 
     backendUrl = 'http://localhost:2000/api/devices'
     network = '192.168.0.0/24'
     portScanner = nmap.PortScanner()
+    knownDevices = None
 
     dbClient = None
-    logging.basicConfig(filename="/var/tmp/deviceScanner.log", level=logging.INFO)
 
-    def __init___(self, network = None):
+    def __init__(self, knownDevices, network, backendURL, loggingFile, logLevel = logging.INFO):
         if network is not None:
             self.network = network
+        if backendURL is not None:
+            self.backendUrl = backendURL
+        if loggingFile is not None:
+            logging.basicConfig(filename=loggingFile, level=logLevel)
+        if knownDevices is not None:
+            self.knownDevices = knownDevices
 
     def initDatabase(self):
         if self.dbClient == None:
@@ -43,9 +51,11 @@ class deviceScanner:
                 device['mac'] = self.portScanner[h]['addresses']['mac']
                 device['ip'] = self.portScanner[h]['addresses']['ipv4']
                 device['vendor'] = self.portScanner[h]['vendor'].get(device['mac'])
-
+                device['knownDevice'] = False
+                if device['mac'] in self.knownDevices:
+                    device['name'] = self.knownDevices[device['mac']]
+                    device['knownDevice'] = True
                 devices.append(device)
-
         return devices
 
     def saveDevice(self, device):
@@ -75,15 +85,41 @@ class deviceScanner:
             return None
 
 if __name__ == '__main__':
-    scanner = deviceScanner()
-    print "Scanning network: ", scanner.network
+
+    network = None
+    backendURL = None
+    loggingFile = None
+
+    with open('settings.json') as data_file:
+        print("Loading settings...")
+        data = json.load(data_file)
+        network = data['local_network']
+        backendURL = data['backend_url']
+        loggingFile = data['logging_file']
+        known_devices = data['devices']
+
+    slack = slackweb.Slack(url=data['slack_url'])
+
+    scanner = deviceScanner(known_devices, network, backendURL, loggingFile, logging.INFO)
+    print("Scanning network: ", scanner.network)
     results = scanner.scan()
+
+    knownDeviceFound = False
     if len(results) > 0:
-        print "Found",len(results),"devices:"
+        print("Found",len(results),"devices:")
         scanner.logInfo(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + ': ' + str(len(results)) + ' devices found')
         for device in results:
-            print "\tDevice:", device['mac'], "@", device['ip'], "(", device['vendor'],")"
+            name = ''
+            if device['knownDevice']:
+                name = "=> " + device['name']
+                knownDeviceFound = True
+            print ("\tDevice:", device['mac'], "@", device['ip'], "(", device['vendor'],")", name)
             result = scanner.saveDevice(device)
+        if knownDeviceFound:
+            print("known device found")
+            slack.notify(text="Known device found. Someone is home!", channel="#home", username="nest-bot", icon_emoji=":snowflake:")
+        else:
+            slack.notify(text="No known devices found. Seems everyone is out.", channel="#home", username="nest-bot", icon_emoji=":snowflake:")
     else:
-        print "No devices found. Are you sure you running with sudo priviledges?"
+        print ("No devices found. Are you sure you running with sudo priviledges?")
         scanner.logError('No devices found')
